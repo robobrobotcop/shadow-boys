83f2459bdf06f498973530f0427f71f409ae22e1
Robin Schwabegger <robin@billogram.com>
2018-09-27 15:06:55 +0200

    remove config file

--- mtg-card-sale/mtg_card_sale_config.py
+++
@@ -1,4 +0,0 @@
-usr = 'steg2robin'
-pwd = 'HlIw7eJWSuz0YCdbImK5S5Opk8shz5u4'
-url = 'https://api.lauri.af/cards/'
-webhook = 'https://hooks.slack.com/services/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'
