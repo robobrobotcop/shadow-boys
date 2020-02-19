e824f09bc3c5a31430441dc88517972a40948a86
Robin Schwabegger <robin@billogram.com>
2020-02-19 14:32:21 +0100

    Do not post empty every other hour

--- uobf/munken.py
+++ uobf/munken.py
@@ -6,7 +6,7 @@ import asyncio
 import datetime
 
 
-webhook = ''
+webhook = 'https://hooks.slack.com/services/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'
 
 time = datetime.datetime.now().strftime("%H")
 sem = asyncio.Semaphore(16)
@@ -26,38 +26,51 @@ async def message():
 
     if time == '10':
         payload = {'text': '@channel Välkomna bröder! Om 10 minuter öppnar det absolut första UÖBF. Jag är Munken och kommer ta hand om er under dagen och uppdatera er om evenemang. Önskar er alla en härlig smakupplevelse!', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '11':
         payload = {'text': '@channel Munken meddelar att det endast är 10 minuter kvar till första evenemanget för dagen. *Vertikaltasting 1*.', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '13':
         payload = {'text': '@channel Dagens andra evenemang gror runt hörnet. Se till att era glas är tomma! *Vertikaltasting 2*.', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '15':
         payload = {'text': '@channel Gammalt är godast, därav kommer det vara ett *blindtest* av gammal gueuze om 10 minuter!', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '18':
         payload = {'text': '@channel Munken har inte fått nog! Spela era dryckesbiljetter väl under *Vertikaltasting 3* om 10 minuter.', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '20':
         payload = {'text': '@channel Vad vore en Öl och Bärs Festival utan en *""Smakupplevelse""*? Vi får se.', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '22':
         payload = {'text': '@channel *Vertikaltasting 4* om tio minuter, vad har vi ens kvar att dricka?', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '23':
         payload = {'text': '@channel Äntligen dags för det vi alla väntat på *Kollens Surprise*. Riktig öl för rktiga munkar!', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
+        return payload
 
     elif time == '00':
         payload = {'text': '@channel Munken tackar för sig och önskar Kollen en god natt. Tack för att ni besökte *UÖBF1*, vi ses nästa år!', 'link_names': 1, 'channel': '#test-messages', 'icon_emoji': ':kapittel:', 'username': 'Munken'}
-    
-    return payload
+        return payload
+
+    else:
+        pass
 
 
 async def main():
     payload = await message()
-
-    await post_call(json.dumps(payload))
+    
+    if payload == '':
+        pass
+    else:
+        await post_call(json.dumps(payload))
 
 
 if __name__ == '__main__':
